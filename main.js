2c58f41ba80dc37ddf5c28a103eace24e750f2ed
serialkiller33 <btech10303.22@bitmesra.ac.in>
2023-12-30 23:47:03 +0530

    30 12 23

--- main.js
+++ main.js
@@ -44,14 +44,3 @@ seeMoreButtons.forEach(button => {
 backButton.onclick = function () {
     carousel.classList.remove('showDetail');
 }
-
-let iconCart = document.querySelector('.icon-cart');
-let closeBtn = document.querySelector('.cartTab .close');
-let body = document.querySelector('body');
-
-iconCart.addEventListener('click', () => {
-    body.classList.toggle('activeTabCart');
-})
-closeBtn.addEventListener('click', () => {
-    body.classList.toggle('activeTabCart');
-})
